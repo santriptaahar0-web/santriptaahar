b6a14257a1afe385b2375a8497887028c98e155a
santriptAahar <santriptaahar0@gmail.com>
2025-12-11 18:58:36 +0530

    Redirect home to coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,7 @@
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-import BannerSection from '@/components/home/BannerSection'
-import BenefitsBar from '@/components/home/BenefitsBar'
-import StatsSection from '@/components/home/StatsSection'
-import WhyChooseUs from '@/components/home/WhyChooseUs'
-import ProductsSection from '@/components/home/ProductsSection'
-import Accomplishments from '@/components/home/Accomplishments'
-import Testimonials from '@/components/home/Testimonials'
-import Newsletter from '@/components/home/Newsletter'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <main className="min-h-screen" style={{ backgroundColor: 'var(--background)' }}>
-      <Header />
-      <BannerSection />
-      <BenefitsBar />
-      <StatsSection />
-      <WhyChooseUs />
-      <ProductsSection />
-      <Accomplishments />
-      <Testimonials />
-      <Newsletter />
-      <Footer />
-    </main>
-  )
+  // Temporarily route all traffic to the Coming Soon experience
+  redirect('/coming-soon')
 }
 
